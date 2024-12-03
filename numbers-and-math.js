d3bc1a7aada14d28ac9f1e94c4ba0d62cff51a2c
Uday Kiran <uday@ceooftech.com>
2024-12-03 18:04:36 +0530

    numbers and math file added

---
+++ numbers-and-math.js
@@ -0,0 +1,59 @@
+// Basics of Math:
+console.log(2 + 2);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 2);
+
+console.log(2 + 2 + 2);
+console.log(2.2 + 2.2);
+
+console.log(10.90 * 2 + 20.95);
+console.log(10.90 * 2 + 20.95 + 4.99);
+
+// Order of operations:
+console.log(1 + 1 * 3);
+console.log((1 + 1) * 3);
+
+console.log(20.95 + 7.99 * 2);
+console.log(36.93 * 0.1);
+console.log((20.95 + 7.99 * 2) * 0.1);
+
+// Inaccuracies with Floats:
+console.log(0.1 + 0.2);
+console.log(20.95 + 7.99);
+console.log((2095 + 799) / 100);
+
+// How to round numbers:
+console.log(Math.round(2.2));
+console.log(Math.round(2.8));
+
+console.log(((2095 + 799) * 0.1) / 100);
+console.log(Math.round((2095 + 799) * 0.1) / 100);
+
+console.log(10 + 3 * 8 + 5);
+
+console.log((10 + 3 * 8 + 5) / 3);
+
+console.log(18.5 + 2 * 7.5);
+
+console.log((18.5 + 2 * 7.5) * 0.1);
+
+console.log((18.5 + 2 * 7.5) * 0.2);
+
+console.log((2095 + 799 + 1899) / 100);
+
+console.log((2095 + 799 + 1899 + 499) / 100);
+
+console.log(Math.round((2095 + 799 + 1899 + 499) * 0.1) / 100);
+
+console.log((2095 + 799 + 1899 + 499) / 100 + Math.round((2095 + 799 + 1899 + 499) * 0.1) / 100);
+
+console.log(Math.floor(2.8));
+
+console.log(Math.ceil(2.2));
+
+console.log((25 * 9 / 5) + 32);
+
+console.log((86 - 32) * 5 / 9);
+
+console.log((-5 * 9 / 5) + 32);
