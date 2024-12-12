4355292cc961132af2f28168f233b07d6644e208
Uday Kiran <uday@ceooftech.com>
2024-12-12 13:11:20 +0530

    html css review, console.log file added

---
+++ html-css-review-console-log.js
@@ -0,0 +1,14 @@
+// This is a comment.
+// This code creates a popup.
+// alert('hello');
+
+/*
+  multi
+  line
+  comment
+*/
+
+console.log(2 + 2);
+console.log('some' + 'text');
+
+console.log('Welcome!');
