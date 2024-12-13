d022d11aca49eb567e853a3e8f27e715de91e3de
Uday Kiran <uday@ceooftech.com>
2024-12-13 20:40:59 +0530

    variables file added

---
+++ variables.js
@@ -0,0 +1,44 @@
+let variable1 = 3;
+console.log(variable1);
+
+const calculation = 2 + 2;
+console.log(calculation);
+console.log(calculation + 2);
+
+const result = calculation + 2;
+console.log(result);
+
+const message = 'hello';
+console.log(message); console.log(';');
+
+variable1 = 5;
+console.log(variable1);
+
+variable1 = variable1 + 1;
+console.log(variable1);
+
+const variable2 = 3;
+var variable3 = 3;
+
+let cartQuantity = 0;
+
+const name = 'Uday';
+
+console.log(`My name is: ${name}`);
+
+const cost1 = 5 + 2 * 3 + 9;
+const cost2 = 423.83 + 2 * 254.30 + 762.90;
+
+console.log(`Cost of food: $${cost1}`);
+console.log(`Cost of food: ₹${cost2}`);
+
+const tax1 = cost1 * 0.1;
+const tax2 = cost2 * 0.1;
+
+console.log(`Tax (10%): $${tax1}`);
+console.log(`Tax (10%): ₹${tax2}`);
+
+const totalCost1 = cost1 + tax1;
+console.log(`Total cost: $${totalCost1}`);
+const totalCost2 = cost2 + tax2;
+console.log(`Total cost: ₹${totalCost2}`);
